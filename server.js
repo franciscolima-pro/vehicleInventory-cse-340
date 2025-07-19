b88bc7bf58f927958cc098d3557fb26561b4fcaa
franciscolima-pro <franciscolimapro@gmail.com>
2025-07-19 01:34:25 -0300

    Add handler errors

--- server.js
+++ server.js
@@ -5,12 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const expressLayouts = require("express-ejs-layouts")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities");
 // const static = require("./routes/static")
 
 
@@ -27,16 +28,39 @@ app.set("layout", "./layouts/layout") // not at views root
 // Static Routes
 app.use(require("./routes/static"))
 // Index Route
-app.get('/', baseController.buildHome)
+app.get('/', utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+/* ***********************
+* File Not Found Route - must be last route in list
+* Place after all routes
+* Unit 3, Basic Error Handling Activity
+**************************/
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+   if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
-
 
 /* ***********************
  * Log statement to confirm server operation
