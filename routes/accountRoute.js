c6784b3bd23ced97837f1a61aa160e167ecba401
franciscolima-pro <franciscolimapro@gmail.com>
2025-07-24 03:16:08 -0300

    Add registration view!

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,4 +5,6 @@ const invController = require("../controllers/accountController");
 
 router.get("/login", utilities.handleErrors(invController.buildLogin));
 
+router.get("/register", utilities.handleErrors(invController.buildRegister));
+
 module.exports = router;
